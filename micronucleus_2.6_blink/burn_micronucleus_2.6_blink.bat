c6e641e0e842f6e1edda3ba2f3c631b684a989a7
xiaolaba <xiao_laba_cn@yahoo.com>
2021-11-12 10:25:45 +0800

    micronucleus_2.6_testing

---
+++ micronucleus_2.6_blink/burn_micronucleus_2.6_blink.bat
@@ -0,0 +1,31 @@
+@echo off
+set path=
+
+
+set mcu=attiny85
+
+set hex=AVR_Attiny85_micronucleus_2.6_blink.hex
+set ac=C:\WinAVR-20100110
+
+set lfuse=0xe1
+
+:: // RESET pin set as IO, no ISP is capable, need HV programmer to reset, be caution
+::::hfuse=0x5d  
+
+:: // pin1, RESET is still available for ISP
+set hfuse=0xdd  
+
+set efuse=0xfe
+
+::2020-08-30 xiaolaba
+
+path %ac%\bin;%ac%\utils\bin;%path%;
+
+:::: burn hex and fuses
+::avrdude -c usbtiny -p %mcu% -U flash:w:%hex%.hex:a -U lfuse:w:%lfuse%:m -U hfuse:w:%hfuse%:m -U efuse:w:%efuse%:m
+
+
+:::: burn hex only
+avrdude -c usbtiny -p %mcu% -U flash:w:%hex%.hex:a
+pause
+:end
