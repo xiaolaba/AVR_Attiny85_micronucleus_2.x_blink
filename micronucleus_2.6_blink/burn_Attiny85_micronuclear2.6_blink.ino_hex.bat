97cb93dc454084da77d5ca6d56f4cbd53aee07a1
xiaolaba <xiao_laba_cn@yahoo.com>
2021-11-12 10:46:14 +0800

    Add files via upload

---
+++ micronucleus_2.6_blink/burn_Attiny85_micronuclear2.6_blink.ino_hex.bat
@@ -0,0 +1,11 @@
+
+set tool=C:\Users\user0\AppData\Local\Arduino15\packages\ATTinyCore\tools\micronucleus\2.5-azd1b/micronucleus
+set hex=Attiny85_micronuclear2.6_blink.ino.hex 
+
+REM C:\Users\user0\AppData\Local\Arduino15\packages\ATTinyCore\tools\micronucleus\2.5-azd1b/micronucleus ^
+
+micronucleus ^
+--no-ansi ^
+--run ^
+--timeout 60 ^
+%hex%
